90939461b64a2116776525c1f42d6028c56e3c94
Aslam Musbar <AslamMusbar@users.noreply.github.com>
2025-05-07 11:27:21 +0530

    index and contact page updated
    index and contact page updated with download resume option and creating a proper django structure with base, nav, footer

--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from portfolio_app import views
 
 urlpatterns = [
-    path('',views.index, name="index")
+    path('',views.index, name="index"),
+    path('contact/',views.contact, name="contact")
 ]
