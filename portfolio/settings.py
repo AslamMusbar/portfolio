7daea55644ee91d3b77ac53b0617678c7e08b44a
Aslam-Thaagam <aslam@thaagam.email>
2026-04-04 12:06:26 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -118,9 +118,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
-]
+STATIC_ROOT = "/home/aslam/www/portfolio/staticfiles"
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR,'static')
+# ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
